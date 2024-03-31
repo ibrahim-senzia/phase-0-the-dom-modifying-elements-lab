ef5956414e81de88642e22e4714e583b3d4b0879
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-31 20:46:08 +0300

    modifying-dom elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const elements = document.getElementById("main");
+const newHeader = document.createElement("h1");
+
+elements.remove();
+//new Header to victory
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.setAttribute('id', 'victory');
